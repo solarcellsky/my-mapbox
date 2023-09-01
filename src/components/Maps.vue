e44a2e27c65675450d7c30fed30a0bdac824f4ab
Solar <solarcellsky@gmail.com>
2023-09-01 17:44:07 +0800

    refactor: 💡 update

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -6,9 +6,14 @@
       :modal-append-to-body="false"
       direction="ltr"
       custom-class="echarts-drawer"
-      size="50%">
+      size="50%"
+    >
       <div class="drawer-content">
-        <model-gltf :src="object.obj" :backgroundAlpha="backgroundAlpha" :lights="lights"></model-gltf>
+        <model-gltf
+          :src="object.obj"
+          :backgroundAlpha="backgroundAlpha"
+          :lights="lights"
+        ></model-gltf>
         <h3>详细信息</h3>
         <p>{{ object.tips }}</p>
         <p>{{ object.origin }}</p>
@@ -20,53 +25,58 @@
 <script>
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
-import { Threebox } from 'threebox-plugin';
-import SunCalc from 'suncalc';
-import 'threebox-plugin/dist/threebox.css';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
-import { ModelGltf } from 'vue-3d-model';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { Threebox } from "threebox-plugin";
+import SunCalc from "suncalc";
+import "threebox-plugin/dist/threebox.css";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import { ModelGltf } from "vue-3d-model";
+import Stats from "three/examples/jsm/libs/stats.module.js";
 
 const naviControl = new mapboxgl.NavigationControl();
 const mapLanguage = new window.MapboxLanguage({ defaultLanguage: "zh-Hans" });
 
 class Custom3DControl {
-  onAdd(map){
+  onAdd(map) {
     this.map = map;
-    this.container = document.createElement('div');
-    this.container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group custom-control';
-    const button = this.$_createButton('control_button')
+    this.container = document.createElement("div");
+    this.container.className =
+      "mapboxgl-ctrl mapboxgl-ctrl-group custom-control";
+    const button = this.$_createButton("control_button");
     this.container.appendChild(button);
     return this.container;
   }
-  onRemove(){
+  onRemove() {
     this.container.parentNode.removeChild(this.container);
     this.map = undefined;
   }
   $_createButton(className) {
-    const el = window.document.createElement('button')
+    const el = window.document.createElement("button");
     el.className = className;
-    el.textContent = '2D';
-    el.addEventListener('click', (e) => {
-      let _text = e.target.innerText == '2D' ? '3D' : '2D';
-      el.textContent = _text;
-      switch(_text) {
-        case '2D':
-          this.map.setPitch(50);
-          this.map.setBearing(-17.6);
-          break;
-        case '3D':
-          this.map.setPitch(0);
-          this.map.setBearing(0);
-          break;
-        default:
-          this.map.setPitch(80);
-          this.map.setBearing(-17.6);
-      }
-      e.stopPropagation()
-    }, false)
+    el.textContent = "2D";
+    el.addEventListener(
+      "click",
+      (e) => {
+        let _text = e.target.innerText == "2D" ? "3D" : "2D";
+        el.textContent = _text;
+        switch (_text) {
+          case "2D":
+            this.map.setPitch(50);
+            this.map.setBearing(-17.6);
+            break;
+          case "3D":
+            this.map.setPitch(0);
+            this.map.setBearing(0);
+            break;
+          default:
+            this.map.setPitch(80);
+            this.map.setBearing(-17.6);
+        }
+        e.stopPropagation();
+      },
+      false
+    );
     return el;
   }
 }
@@ -77,35 +87,140 @@ const stats = new Stats();
 export default {
   name: "Maps",
   components: {
-    ModelGltf
+    ModelGltf,
   },
   data() {
     return {
-      accessToken: "pk.eyJ1Ijoic29sYXJjZWxsc2t5IiwiYSI6ImNrbmFmOXVjdjExZ3oycnRhY3ZqNWJqNHcifQ.INN_v0VVzC2KNQVL-nvMLg", // your access token. Needed if you using Mapbox 
+      accessToken:
+        "pk.eyJ1Ijoic29sYXJjZWxsc2t5IiwiYSI6ImNrbmFmOXVjdjExZ3oycnRhY3ZqNWJqNHcifQ.INN_v0VVzC2KNQVL-nvMLg", // your access token. Needed if you using Mapbox
       mapStyle: "mapbox://styles/mapbox/dark-v10", // your map style
       drawer: false,
-      coordinates: [
-        [108.986133,34.275893]
-      ],
+      coordinates: [[108.986133, 34.275893]],
       minZoom: 10,
       backgroundAlpha: 0,
       object: {},
-      lights: [ 
-         { 
-           type: 'HemisphereLight', 
-           position: { x: 0, y: 1, z: 0 }, 
-           skyColor: 0xffffff, 
-           groundColor: 0xeeeeee, 
-           intensity: 0.2, 
-         }, 
-         { 
-           type: 'DirectionalLight', 
-           position: { x: 1, y: 1, z: 1 }, 
-           color: 0xffffff, 
-           intensity: 0.8, 
-         }, 
-       ]
-    }
+      models: [
+        {
+          id: "34M_17",
+          geojson: "",
+          extrusion: false,
+          sourceId: "34M_17",
+          obj: "./assets/models/34M17/34M_17.gltf",
+          origin: [116.38023, 39.913302],
+          type: "gltf",
+          scale: 3.5,
+          tips: "雷达",
+          units: "meters",
+          rotation: { x: 90, y: 0, z: 0 },
+        },
+        {
+          id: "eiffel",
+          geojson: "",
+          extrusion: false,
+          sourceId: "eiffel",
+          obj: "./assets/models/landmarks/eiffel.glb",
+          origin: [108.984133, 34.275893],
+          type: "gltf",
+          scale: 6000,
+          tips: "Eiffel Tower",
+          units: "meters",
+          rotation: { x: 90, y: 0, z: 0 },
+        },
+        {
+          id: "Duck",
+          geojson: "",
+          extrusion: false,
+          sourceId: "Duck",
+          obj: "./assets/models/gltf/Duck/glTF/Duck.gltf",
+          origin: [108.981133, 34.275893],
+          type: "gltf",
+          scale: 50,
+          tips: "小黄鸭",
+          units: "meters",
+          rotation: { x: 90, y: 0, z: 0 },
+        },
+        {
+          id: "plane",
+          geojson: "",
+          extrusion: false,
+          sourceId: "plane",
+          obj: "./assets/models/plane/plane.glb",
+          origin: [108.984119, 34.270911],
+          type: "gltf",
+          scale: 30,
+          tips: "Plane",
+          units: "meters",
+          rotation: { x: 90, y: 0, z: 0 },
+        },
+        {
+          id: "Soldier",
+          geojson: "",
+          extrusion: false,
+          sourceId: "Soldier",
+          obj: "./assets/models/Soldier.glb",
+          origin: [108.975133, 34.275893],
+          type: "gltf",
+          scale: 150,
+          tips: "Soldier",
+          units: "meters",
+          rotation: { x: 90, y: 0, z: 0 },
+        },
+        {
+          id: "libertystatue",
+          geojson: "./assets/geojson/statue-of-liberty.geojson",
+          extrusion: false,
+          sourceId: "libertystatue",
+          obj: "./assets/models/landmarks/libertystatue.glb",
+          origin: [108.985212, 34.266347, 0],
+          type: "gltf",
+          scale: 666,
+          tips: "Statue of Liberty",
+          units: "meters",
+          rotation: { x: 90, y: 0, z: 0 },
+        },
+        {
+          id: "elephant",
+          geojson: "",
+          extrusion: false,
+          sourceId: "elephant",
+          obj: "./assets/models/history/elephant.glb",
+          origin: [108.980006, 34.268553],
+          type: "gltf",
+          scale: 150,
+          tips: "Elephant",
+          units: "meters",
+          rotation: { x: 90, y: 0, z: 0 },
+        },
+        {
+          id: "seraphine",
+          geojson: "",
+          extrusion: false,
+          sourceId: "seraphine",
+          obj: "./assets/models/seraphine/scene.gltf",
+          origin: [108.981133, 34.266553],
+          type: "gltf",
+          scale: 150,
+          tips: "Seraphine",
+          units: "meters",
+          rotation: { x: 90, y: 0, z: 0 },
+        },
+      ],
+      lights: [
+        {
+          type: "HemisphereLight",
+          position: { x: 0, y: 1, z: 0 },
+          skyColor: 0xffffff,
+          groundColor: 0xeeeeee,
+          intensity: 0.2,
+        },
+        {
+          type: "DirectionalLight",
+          position: { x: 1, y: 1, z: 1 },
+          color: 0xffffff,
+          intensity: 0.8,
+        },
+      ],
+    };
   },
   mounted() {
     this.initMap();
@@ -131,48 +246,48 @@ export default {
         zoom: 14.5,
         pitch: 80,
         bearing: -17.6,
-        minZoom: 0
+        minZoom: 0,
       });
 
       let Geocoder = new MapboxGeocoder({
         accessToken: this.accessToken,
         marker: {
-          color: '#ff0011'
+          color: "#ff0011",
         },
-        country: 'CN',
-        language: 'zh-Hans',
-        mapboxgl: mapboxgl
+        country: "CN",
+        language: "zh-Hans",
+        mapboxgl: mapboxgl,
       });
 
       map.addControl(naviControl, "bottom-right");
       map.addControl(custom3DControl, "bottom-right");
       map.addControl(mapLanguage);
-      map.addControl(Geocoder, 'top-left');
+      map.addControl(Geocoder, "top-right");
 
       window.$map = map;
 
-      map.on('load', () => {
+      map.on("load", () => {
         map.getContainer().appendChild(stats.dom);
         self.animate();
         map.addLayer({
-          id: 'sky-layer',
-          type: 'sky',
+          id: "sky-layer",
+          type: "sky",
           paint: {
-          'sky-opacity': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            0,
-            0,
-            5,
-            0.3,
-            8,
-            1
-          ],
-          'sky-type': 'atmosphere',
-          'sky-atmosphere-sun': self.getSunPosition(null, map),
-          'sky-atmosphere-sun-intensity': 5
-          }
+            "sky-opacity": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              0,
+              0,
+              5,
+              0.3,
+              8,
+              1,
+            ],
+            "sky-type": "atmosphere",
+            "sky-atmosphere-sun": self.getSunPosition(null, map),
+            "sky-atmosphere-sun-intensity": 5,
+          },
         });
 
         // map.addSource('mapbox-dem', {
@@ -188,115 +303,11 @@ export default {
         //   color: 'white',
         //   'horizon-blend': 0.3
         // });
-      })
+      });
 
-      map.on('style.load', () => {
-        this.addThreeDModel(map, [{
-          id: '34M_17',
-          geojson: '',
-          extrusion: false,
-          sourceId: '34M_17',
-          obj: './assets/models/34M17/34M_17.gltf',
-          origin: [108.988133, 34.275893],
-          type: 'gltf',
-          scale: 3.5,
-          tips: "雷达",
-          units: 'meters',
-          rotation: { x: 90, y: 0, z: 0 }
-        },
-        {
-          id: 'eiffel',
-          geojson: '',
-          extrusion: false,
-          sourceId: 'eiffel',
-          obj: './assets/models/landmarks/eiffel.glb',
-          origin: [108.984133, 34.275893],
-          type: 'gltf',
-          scale: 6000,
-          tips: "Eiffel Tower",
-          units: 'meters',
-          rotation: { x: 90, y: 0, z: 0 }
-        },
-        {
-          id: 'Duck',
-          geojson: '',
-          extrusion: false,
-          sourceId: 'Duck',
-          obj: './assets/models/gltf/Duck/glTF/Duck.gltf',
-          origin: [108.981133, 34.275893],
-          type: 'gltf',
-          scale: 50,
-          tips: "小黄鸭",
-          units: 'meters',
-          rotation: { x: 90, y: 0, z: 0 }
-        },
-        {
-          id: 'plane',
-          geojson: '',
-          extrusion: false,
-          sourceId: 'plane',
-          obj: './assets/models/plane/plane.glb',
-          origin: [108.984119, 34.270911],
-          type: 'gltf',
-          scale: 30,
-          tips: "Plane",
-          units: 'meters',
-          rotation: { x: 90, y: 0, z: 0 }
-        },
-        {
-          id: 'Soldier',
-          geojson: '',
-          extrusion: false,
-          sourceId: 'Soldier',
-          obj: './assets/models/Soldier.glb',
-          origin: [108.975133, 34.275893],
-          type: 'gltf',
-          scale: 150,
-          tips: "Soldier",
-          units: 'meters',
-          rotation: { x: 90, y: 0, z: 0 }
-        },
-        {
-          id: 'libertystatue',
-          geojson: './assets/geojson/statue-of-liberty.geojson',
-          extrusion: false,
-          sourceId: 'libertystatue',
-          obj: './assets/models/landmarks/libertystatue.glb',
-          origin: [108.985212, 34.266347, 0],
-          type: 'gltf',
-          scale: 666,
-          tips: "Statue of Liberty",
-          units: 'meters',
-          rotation: { x: 90, y: 0, z: 0 }
-        },
-        {
-          id: 'elephant',
-          geojson: '',
-          extrusion: false,
-          sourceId: 'elephant',
-          obj: './assets/models/history/elephant.glb',
-          origin: [108.980006, 34.268553],
-          type: 'gltf',
-          scale: 150,
-          tips: "Elephant",
-          units: 'meters',
-          rotation: { x: 90, y: 0, z: 0 }
-        },
-        {
-          id: 'seraphine',
-          geojson: '',
-          extrusion: false,
-          sourceId: 'seraphine',
-          obj: './assets/models/seraphine/scene.gltf',
-          origin: [108.981133, 34.266553],
-          type: 'gltf',
-          scale: 150,
-          tips: "Seraphine",
-          units: 'meters',
-          rotation: { x: 90, y: 0, z: 0 }
-        }]);
-      })
-
+      map.on("style.load", () => {
+        this.addThreeDModel(map, this.models);
+      });
     },
     getSunPosition(date, map) {
       let center = map.getCenter();
@@ -311,52 +322,58 @@ export default {
     },
     addThreeDModel(map, options) {
       let self = this;
-      window.tb = new Threebox(
-        map,
-        map.getCanvas().getContext('webgl'),
-        {
-          realSunlight: true,
-          sky: false,
-          enableSelectingFeatures: true,
-          enableSelectingObjects: true,
-          enableDraggingObjects: true,
-          enableRotatingObjects: true,
-          enableTooltips: true, // change this to false to disable default tooltips on fill-extrusion and 3D models
-        }
-      );
+      window.tb = new Threebox(map, map.getCanvas().getContext("webgl"), {
+        realSunlight: true,
+        sky: false,
+        enableSelectingFeatures: true,
+        enableSelectingObjects: true,
+        enableDraggingObjects: true,
+        enableRotatingObjects: true,
+        enableTooltips: true, // change this to false to disable default tooltips on fill-extrusion and 3D models
+      });
 
-      options.map(function(option) {
+      options.map(function (option) {
         if (option.extrusion) {
-          self.createExtrusionLayer(map, option.geojson, option.sourceId + '-source', option.id + '-extrusion');
+          self.createExtrusionLayer(
+            map,
+            option.geojson,
+            option.sourceId + "-source",
+            option.id + "-extrusion"
+          );
         }
-      })
+      });
 
       let tb = window.tb;
       let date = new Date();
       map.addLayer({
-        id: 'custom-3d-models',
-        type: 'custom',
-        renderingMode: '3d',
+        id: "custom-3d-models",
+        type: "custom",
+        renderingMode: "3d",
         onAdd: function () {
-          options.map(function(option) {
+          options.map(function (option) {
             tb.loadObj(option, function (model) {
+              console.log(model)
               model.setCoords(option.origin);
               model.setRotation({ x: 0, y: 0, z: 80 });
               model.addTooltip(option.tips, true);
               model.castShadow = true;
               tb.add(model);
               if (model) {
-                model.addEventListener('SelectedChange', self.onSelectedChange, false);
+                model.addEventListener(
+                  "SelectedChange",
+                  self.onSelectedChange,
+                  false
+                );
               }
-            })
-          })
+            });
+          });
         },
 
         render: () => {
           tb.setSunlight(date, options.origin);
           tb.update();
-        }
-      })
+        },
+      });
     },
     onSelectedChange(e) {
       // let selected = e.detail.selected;
@@ -376,85 +393,84 @@ export default {
       });
 
       let layer = {
-        'id': layerId,
-        'source': sourceId,
-        'filter': ['==', 'extrude', 'true'],
-        'type': 'fill-extrusion',
-        'minzoom': this.minZoom,
-        'paint': {
-          'fill-extrusion-color':
-            [
-              'case',
-              ['boolean', ['feature-state', 'select'], false],
-              "lightgreen",
-              ['boolean', ['feature-state', 'hover'], false],
-              "lightblue",
-              '#C7B299'
-            ],
+        id: layerId,
+        source: sourceId,
+        filter: ["==", "extrude", "true"],
+        type: "fill-extrusion",
+        minzoom: this.minZoom,
+        paint: {
+          "fill-extrusion-color": [
+            "case",
+            ["boolean", ["feature-state", "select"], false],
+            "lightgreen",
+            ["boolean", ["feature-state", "hover"], false],
+            "lightblue",
+            "#C7B299",
+          ],
           // use an 'interpolate' expression to add a smooth transition effect to the
           // buildings as the user zooms in
-          'fill-extrusion-height': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
+          "fill-extrusion-height": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
             this.minZoom,
             0,
             this.minZoom + 0.05,
-            ['get', 'height']
+            ["get", "height"],
           ],
-          'fill-extrusion-base': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
+          "fill-extrusion-base": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
             this.minZoom,
             0,
             this.minZoom + 0.05,
-            ['get', 'min_height']
+            ["get", "min_height"],
           ],
-          'fill-extrusion-opacity': 1
-        }
+          "fill-extrusion-opacity": 1,
+        },
       };
       map.addLayer(layer);
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style>
 ::-webkit-scrollbar {
-    height: 5px;
-    width: 10px;
-    border-radius: 10px;
+  height: 5px;
+  width: 10px;
+  border-radius: 10px;
 }
 
 ::-webkit-scrollbar-button:vertical {
-    display: none
+  display: none;
 }
 
 ::-webkit-scrollbar-corner:vertical {
-    background-color: #0f0d2a;
-    border-radius: 10px;
+  background-color: #0f0d2a;
+  border-radius: 10px;
 }
 
 ::-webkit-scrollbar-resizer:vertical {
-    background-color: #ff6e00
+  background-color: #ff6e00;
 }
 
 ::-webkit-scrollbar-thumb:vertical {
-    background-color: #8e8e8e
+  background-color: #8e8e8e;
 }
 
 ::-webkit-scrollbar-thumb:vertical:hover {
-    background-color: #0099ff
+  background-color: #0099ff;
 }
 
 ::-webkit-scrollbar-track-piece {
-    background: #ccc
+  background: #ccc;
 }
 
 ::-webkit-scrollbar-track:vertical {
-    background-color: #000;
-    border-radius: 10px;
+  background-color: #000;
+  border-radius: 10px;
 }
 #mapView {
   width: 100%;
@@ -488,7 +504,7 @@ canvas:focus-visible {
   color: #eeeeee !important;
 }
 .el-drawer.echarts-drawer {
-  background: rgba(0, 0, 0, .6) !important;
+  background: rgba(0, 0, 0, 0.6) !important;
   height: 100%;
   overflow: hidden;
 }
